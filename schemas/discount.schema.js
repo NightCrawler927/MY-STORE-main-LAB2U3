3fb497aaffb259ef349a295bd1ce6bf6d778bec8
NightCrawler927 <nelsonbarahona2011@gmail.com>
2025-03-04 15:33:58 -0500

    final

--- schemas/discount.schema.js
+++ schemas/discount.schema.js
@@ -13,12 +13,15 @@ const price_max = Joi.number().integer();
 const limit = Joi.number().integer();
 const offset = Joi.number().integer();
 
+const discountId = Joi.number().integer(); // Definimos discountId como opcional
+
 const createProductSchema = Joi.object({
   name: name.required(),
   price: price.required(),
   description: description.required(),
   image: image.required(),
   categoryId: categoryId.required(),
+  discountId: discountId, // discountId opcional
 });
 
 const updateProductSchema = Joi.object({
@@ -27,6 +30,7 @@ const updateProductSchema = Joi.object({
   description: description,
   image: image,
   categoryId: categoryId,
+  discountId: discountId, // discountId opcional en actualización
 });
 
 const getProductSchema = Joi.object({
@@ -39,6 +43,7 @@ const queryProductSchema = Joi.object({
   price,
   price_min,
   price_max,
+  discountId: discountId, // discountId también opcional en consultas
 });
 
 module.exports = {
@@ -47,4 +52,3 @@ module.exports = {
   getProductSchema,
   queryProductSchema,
 };
-
